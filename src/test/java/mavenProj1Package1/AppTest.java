d1fc631fe12780f2506785e79b8de7077c37b201
jenkins <jenkins@localhost.localdomain>
2020-09-30 12:55:09 +0530

    first commit

---
+++ src/test/java/mavenProj1Package1/AppTest.java
@@ -0,0 +1,20 @@
+package mavenProj1Package1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
